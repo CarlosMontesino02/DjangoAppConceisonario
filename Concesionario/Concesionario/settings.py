d94a1c3763d6e99c09d97fd20a3cdced15cbb9fc
Carlos Montesino Fernando <72274931+CarlosMontesino02@users.noreply.github.com>
2022-01-13 15:59:10 +0100

    media imagenes fix

--- Concesionario/Concesionario/settings.py
+++ Concesionario/Concesionario/settings.py
@@ -118,9 +118,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+
+STATIC_ROOT = ""
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Path where media is stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+MEDIA_URL = '/media/'
