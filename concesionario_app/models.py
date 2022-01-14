080745e80b7dabfbb49f124bd1cdb4579ac88424
Carlos Montesino Fernando <72274931+CarlosMontesino02@users.noreply.github.com>
2022-01-14 20:57:25 +0100

    Update models.py

--- concesionario_app/models.py
+++ concesionario_app/models.py
@@ -1,11 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Usuario(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     DNI = models.CharField(max_length=9)
     NumTlf = models.IntegerField()
     Direccion = models.CharField(max_length=50)
     def __str__(self):
         return self.DNI
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 class Concesionario(models.Model):
     Ubicacion = models.CharField(max_length=60)
@@ -20,9 +32,8 @@ class Coche(models.Model):
     Modelo = models.CharField(max_length=30)
     Color = models.CharField(max_length=11)
     Extras = models.CharField(max_length=100)
-    Stock = models.CharField(max_length=10)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
-    upload = models.ImageField(upload_to ='uploads/')
+    upload = models.ImageField(upload_to ='img/')
     descripcion  = models.CharField(max_length=300)
     def __str__(self):
         return self.Modelo
@@ -33,4 +44,3 @@ class Carrito(models.Model):
     def __str__(self):
         return str(self.Usuario)
 # Create your models here.
-
