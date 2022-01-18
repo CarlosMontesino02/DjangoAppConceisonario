742c4f9f2d414ddfb0f4285344258a9fde5b1327
Carlos Montesino Fernando <72274931+CarlosMontesino02@users.noreply.github.com>
2022-01-18 20:54:13 +0100

    Update admin.py

--- concesionario_app/admin.py
+++ concesionario_app/admin.py
@@ -3,8 +3,18 @@ from .models import Usuario
 from .models import Concesionario
 from .models import Coche
 from .models import Carrito
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from concesionario_app.models import Usuario
+
+# Define an inline admin descriptor for Employee model
+# which acts a bit like a singleton
+# Define a new User admin
+# Re-register UserAdmin
+# Register your models here.
+
 admin.site.register(Usuario)
 admin.site.register(Concesionario)
 admin.site.register(Coche)
 admin.site.register(Carrito)
-# Register your models here.
