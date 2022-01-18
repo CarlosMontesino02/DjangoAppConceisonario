4e6c163e6d6203a2760e11f84d34be16847121e6
Carlos Montesino Fernando <72274931+CarlosMontesino02@users.noreply.github.com>
2022-01-18 20:54:51 +0100

    Update urls.py

--- Concesionario/Concesionario/urls.py
+++ Concesionario/Concesionario/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('', index),
     path('concesionarios/', concesionarios.as_view(), name="concesionarios"),
     path('carritos/', carritos.as_view(), name="carrito"),
+    #path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
