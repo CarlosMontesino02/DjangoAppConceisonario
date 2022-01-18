6b35921e77e0f6c57b9c7ec48205a282338ae540
Carlos Montesino Fernando <72274931+CarlosMontesino02@users.noreply.github.com>
2022-01-18 20:53:37 +0100

    Update models.py

--- concesionario_app/models.py
+++ concesionario_app/models.py
@@ -4,20 +4,11 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 class Usuario(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
     DNI = models.CharField(max_length=9)
     NumTlf = models.IntegerField()
     Direccion = models.CharField(max_length=50)
     def __str__(self):
         return self.DNI
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
 
 class Concesionario(models.Model):
     Ubicacion = models.CharField(max_length=60)
