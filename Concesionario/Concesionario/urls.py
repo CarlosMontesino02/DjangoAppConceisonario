c50b8c1af65f5f2b3eea96293c264c3ed7c628bc
Carlos Montesino Fernando <72274931+CarlosMontesino02@users.noreply.github.com>
2022-01-28 21:37:00 +0100

    Update urls.py

--- Concesionario/Concesionario/urls.py
+++ Concesionario/Concesionario/urls.py
@@ -16,31 +16,27 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from concesionario_app.views import carritos, ofertadecoches, index, perfiles, concesionarios
-from concesionario_app.views import carritos_detalles, ofertadecoches_detalles, index, perfiles_detalles
-from concesionario_app.views import concesionarios_detalles, cochecreateview, registro, cocheUpdateView
+from concesionario_app.views import carritos_detalles, ofertadecoches_detalles, perfiles_detalles, concesionarios_detalles
+from concesionario_app.views import concesionarios_detalles, cochecreateview, registro, cocheUpdateView, concesionariocreateview, concesionarioUpdateView
 from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path
-from concesionario_app.views import AuthorCreateView, AuthorDeleteView, AuthorUpdateView
 
 urlpatterns = [
     path('perfil/add',registro.as_view(), name="regsitro"),
     path('coches/add',cochecreateview.as_view(), name="coches-add"),
     path('coches/<int:pk>/update', cocheUpdateView.as_view(), name='coche-update'),
+    path('concesionarios/add',concesionariocreateview.as_view(), name="concesionario-add"),
+    path('concesionarios/<int:pk>/update', concesionarioUpdateView.as_view(), name='concesionario-update'),
     path('admin/', admin.site.urls),
     path('coches/', ofertadecoches.as_view(), name="coches"),
-    path('', index),
+    path('', index, name="index"),
     path('concesionarios/', concesionarios.as_view(), name="concesionarios"),
     path('carritos/', carritos.as_view(), name="carrito"),
     path('perfiles/', perfiles.as_view(), name="perfiles"),
-    path('concesionarios_detalles/', concesionarios_detalles.as_view(), name="concesionarios_detalles"),
+    path('concesionarios/<int:pk>/', concesionarios_detalles.as_view(), name='concesionarios_detalles'),
     path('carritos_detalles/', carritos_detalles.as_view(), name="carrito_detalles"),
     path('perfiles_detalles/', perfiles_detalles.as_view(), name="perfiles_detalles"),
     #path('coches_detalles/', ofertadecoches_detalles.as_view(), name="coches_detalles"),
     path('coches/<int:pk>/', ofertadecoches_detalles.as_view(), name='coches_detalles'),
-
-    path('author/', perfiles.as_view(), name="author"),
-    path('author/add/', AuthorCreateView.as_view(), name='author-add'),
-    path('author/<int:pk>/', AuthorUpdateView.as_view(), name='author-update'),
-    path('author/<int:pk>/delete/', AuthorDeleteView.as_view(), name='author-delete'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
